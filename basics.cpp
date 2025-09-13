dd06f6d1b290362dc40ce79d3b3f0754b70a1251
DEV-PATEL <DEVP59237@GMAIL.com>
2025-09-14 01:42:08 +0530

    day 1 end line and comments

---
+++ basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    //this is a simple C++ program that prints "Hello, World!" to the console.
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "its really awesome" <<'\n';
+    return 0;
+}
